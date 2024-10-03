1b5ee2f3cd4c285aa9f729828f96ce626ed59422
Sathiyapramod <sathiyapramod22@gmail.com>
2024-10-03 11:06:42 +0530

    added memoize with useMemo

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,14 @@ import { RouteObject, useRoutes } from 'react-router-dom';
 import CustomerRouter from './CustomersRouter';
 import InvoicesRouter from './InvoicesRouter';
 import TeamRouter from './TeamRouter';
+import { useMemo } from 'react';
 
 function Router() {
-  const myRoutes: RouteObject[] = [CustomerRouter, InvoicesRouter, TeamRouter];
-  return useRoutes(myRoutes);
+  const customRoutes = useMemo(()=>{
+    const myRoutes: RouteObject[] = [CustomerRouter, InvoicesRouter, TeamRouter];
+    return myRoutes;
+  },[])
+  return useRoutes(customRoutes);
 }
 
 export default Router;
